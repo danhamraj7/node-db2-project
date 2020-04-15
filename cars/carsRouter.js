969c81af11b7110a855149b59b93c61344ba34d9
danhamraj <danhamraj@gmail.com>
2020-04-14 21:41:48 -0400

    created all endpoints for carsRouter, all CRUD running

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -16,4 +16,78 @@ router.get("/", (req, res) => {
     );
 });
 
+// get a car by car id
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  cars
+    .findById(id)
+    .then((car) => {
+      if (car) {
+        res.json(car);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find the car with given id." });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get car" });
+    });
+});
+
+// add a car
+router.post("/", (req, res) => {
+  const newCar = req.body;
+  //console.log(newCar);
+  cars
+    .insert(newCar)
+    .then((result) => res.status(201).json(result))
+    .catch((err) =>
+      res.status(400).json({
+        errorMessage:
+          "Please provide VIN, make, model, milage, transmission, title for the car.",
+      })
+    );
+});
+
+//update a car
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  cars
+    .findById(id)
+    .then((car) => {
+      if (car) {
+        cars.update(changes, id).then((updatedCar) => {
+          res.json({ updated: updatedCar });
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find the car with given id" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to update car" });
+    });
+});
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  cars
+    .remove(id)
+    .then((deleted) => {
+      if (deleted) {
+        res.json({ removed: deleted });
+      } else {
+        res.status(404).json({ message: "Could not find car with given id" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to delete car" });
+    });
+});
+
 module.exports = router;
